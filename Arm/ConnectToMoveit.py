8ef856403ff509539d6351ebdd7a9bda025fe550
ieqefcr <ieqefcr@hotmail.com>
2024-03-02 00:15:40 +0800

    update connect

--- Arm/ConnectToMoveit.py
+++ Arm/ConnectToMoveit.py
@@ -4,6 +4,7 @@ import numpy as np
 import roslibpy
 import time
 from math import sin, cos
+import os
 
 plc = pyads.Connection('192.168.0.182.1.1', 851)
 plc.open()
@@ -54,34 +55,44 @@ def move():
 
 def receive_message():
     listener = roslibpy.Topic(client, '/arm', 'std_msgs/Float64MultiArray')
-    message_data = [None]
-    for i in range(6):
-        angle[i] = plc.get_symbol("MAIN.Pos"+str(i+1),auto_update=True)
-    while 1:
-        print(angle[0].value)
-        listener.subscribe(
-            lambda message: message_data.__setitem__(0, message['data']))
+    publisher = roslibpy.Topic(
+        client, '/arm_state', 'std_msgs/Float64MultiArray')
 
-        if message_data[0] is not None:
-            print(message_data[0])
-            index = 0
-            for i in message_data[0]:
-                if index % 6 == 4 or index % 6 == 3:
-                    plc.write_by_name(
-                        "MAIN.Pos_arr2["+str(index//6)+","+str(index % 6+1)+"]", -i)
-                else:
-                    plc.write_by_name(
-                        "MAIN.Pos_arr2["+str(index//6)+","+str(index % 6+1)+"]", i)
-                index = index + 1
+    pid = os.fork()
 
-            last_angle = message_data[0][-6:]
+    if pid == 0:
+        message_data = [None]
+        while 1:
+            print(angle[0].value)
+            listener.subscribe(
+                lambda message: message_data.__setitem__(0, message['data']))
 
-            print(index//6)
-            plc.write_by_name("MAIN.pos_num", index//6)
-            move()
-            message_data[0] = None
-        # listener.unsubscribe()
-        # print(message)
+            if message_data[0] is not None:
+                print(message_data[0])
+                index = 0
+                for i in message_data[0]:
+                    if index % 6 == 4 or index % 6 == 3:
+                        plc.write_by_name(
+                            "MAIN.Pos_arr2["+str(index//6)+","+str(index % 6+1)+"]", -i)
+                    else:
+                        plc.write_by_name(
+                            "MAIN.Pos_arr2["+str(index//6)+","+str(index % 6+1)+"]", i)
+                    index = index + 1
+
+                last_angle = message_data[0][-6:]
+
+                print(index//6)
+                plc.write_by_name("MAIN.pos_num", index//6)
+                move()
+                message_data[0] = None
+            # listener.unsubscribe()
+            # print(message)
+    else:
+        for i in range(6):
+            angle[i] = plc.get_symbol("MAIN.Pos"+str(i+1), auto_update=True)
+        while 1:
+            publisher.publish(roslibpy.Message({'data': angle}))
+            time.sleep(0.1)
 
 
 client.run()
