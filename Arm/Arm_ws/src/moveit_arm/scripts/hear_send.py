0282068c58a5439c05bb16d9da37c6ba3edaa8f9
ieqefcr <ieqefcr@hotmail.com>
2024-03-03 16:41:13 +0800

    add joint_state callback

--- Arm/Arm_ws/src/moveit_arm/scripts/hear_send.py
+++ Arm/Arm_ws/src/moveit_arm/scripts/hear_send.py
@@ -8,6 +8,7 @@ from std_msgs.msg import Float64MultiArray
 from std_msgs.msg import String
 from moveit_msgs.msg import RobotTrajectory
 from moveit_msgs.msg import DisplayTrajectory
+from sensor_msgs.msg import JointState
 
 def callback(data):
     # print(data.trajectory[0].joint_trajectory.points)
@@ -25,15 +26,27 @@ def callback(data):
             # rospy.sleep(0.05)
             # pub.publish(msg)
 
+def arm_state_callback(data):
+    global arm_state_pub
+    angle_list = data.data.split(',')
+    arm_state_msg.position = [float(i) for i in angle_list]
+    arm_state_msg.velocity = [0,0,0,0,0,0]
+    arm_state_msg.effort = [0,0,0,0,0,0]
+    arm_state_msg.header.stamp = rospy.Time.now()
+    arm_state_pub.publish(arm_state_msg)
+
+
 def get_plan():
     sub = rospy.Subscriber('/move_group/display_planned_path', DisplayTrajectory, callback)
-
+    arm_state = rospy.Subscriber('/arm_state', String, arm_state_callback)
     rospy.spin()
 
 if __name__ == "__main__":
     rospy.init_node('hear_send')
     launch_time = rospy.Time.now()
     pub = rospy.Publisher('/arm', Float64MultiArray, queue_size=10)
+    arm_state_pub = rospy.Publisher('/move_group/fake_controller_joint_states', JointState, queue_size=10)
     msg = Float64MultiArray()
+    arm_state_msg = JointState()
     rospy.Rate(5)
     get_plan()
