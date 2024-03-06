a436c28c0f6df86c9f0ad91411869e46df355efd
ieqefcr <ieqefcr@hotmail.com>
2024-03-06 18:57:43 +0800

    update imitation learning

--- imitation_learning/train.py
+++ imitation_learning/train.py
@@ -7,119 +7,113 @@ from PIL import Image
 import cv2
 import pyaml
 import os
+from tqdm import tqdm
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 def generate_random_dataset(dataset_path):
-    train_img = []
-    train_label = []
-    for i in range(10):
-        img = np.random.randint(0, 255, size=(480, 640, 3))
-        # resize img to 256*256
-        img = img.astype(np.uint8)
-        img = cv2.resize(img, (256, 256))
-        # cv2.imshow("img", img)
-        cv2.imwrite( 'dataset/'+str(i) + '.jpg', img)
-        print(dataset_path + '/' + str(i) + '.jpg')
-        # cv2.waitKey(0)
-        # img 2 tensor
-        train_img.append(img)
-        train_label.append(np.random.randn(3))
-        print(train_label[-1])
-    train_img = np.asarray(train_img)
-    train_label = np.asarray(train_label)
-    return train_img, train_label
 
+    max_folder = -1
+    for folder in os.listdir(dataset_path):
+        if int(folder) > max_folder:
+            max_folder = int(folder)
+    max_folder += 1
+    os.mkdir(os.path.join(dataset_path, str(max_folder)))
+    print("save random dataset to " + dataset_path)
+
+    for i in tqdm(range(20), desc="Generating random dataset"):
+        c1 = np.random.randint(0, 255, size=(480, 640, 1))
+        c2 = np.random.randint(0, 255, size=(480, 640, 1))
+        c3 = np.random.randint(0, 255, size=(480, 640, 1))
+        img = np.concatenate((c1, c2, c3), axis=2) # 480x640x3
+        label = np.random.randn(3)
+        label = np.round(label, 3)
+        label_name = str(i)+'_'+str(label[0]) + '_' + str(label[1]) + '_' + str(label[2]) + '.jpg'
+        cv2.imwrite(os.path.join(dataset_path, str(max_folder), label_name), img)
 
 class CustomDataset(Dataset):  # 输入为图片路径和标签
-    def __init__(self, train_img, train_label, transform=None):
-        # self.data_path = data_path
+    def __init__(self, dataset_path, transform=None):
+        self.dataset_path = dataset_path
         self.transform = transform
-        # self.imgs = []
-        # self.labels = []
-        # with open(data_path + '/train.txt', 'r') as f:
-        #     for line in f.readlines():
-        #         img_path, label = line.strip().split()
-        #         self.imgs.append(img_path)
-        #         self.labels.append(int(label))
-        self.imgs = train_img
-        self.labels = train_label
+        self.imgs = []
+        self.labels = []
+        for folder in os.listdir(dataset_path):
+            #open the folder, get the image
+            for file in os.listdir(os.path.join(dataset_path, folder)):
+                img = Image.open(os.path.join(dataset_path, folder, file))
+                label = file.split('_')   #name structure: index_roll1_roll2_grip.jpg
+                label = label[1:4]          #roll1, roll2, grip
+                label[2] = label[2][:-4]    #remove.jpg
+                label = [float(i) for i in label]
+                # print(label)
+                self.imgs.append(img)
+                self.labels.append(label)
         self.data_len = len(self.imgs)
 
     def __getitem__(self, index):
         img = self.imgs[index]
         label = self.labels[index]
-        # scalar type
-        # exchange the channel
-        img = np.transpose(img, (2, 0, 1))
-        img = torch.from_numpy(img)
-        # expected scalar type Byte but found Float
-        # if self.transform is not None:
-        # img = self.transform(img)
+        if self.transform is not None:
+            img = self.transform(img)
+        label = torch.tensor(label)
         return img, label
 
     def __len__(self):
         return self.data_len
 
 
+
 def load_dataset():
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
-    #print now path
-
     config = pyaml.yaml.load(open('./config.yaml'),
                              Loader=pyaml.yaml.SafeLoader)
     dataset_path = config['dataset_path']
-    # 找到数据集路径中最大的文件夹
-    max_folder = -1
-    for folder in os.listdir(dataset_path):
-        if int(folder) > max_folder:
-            max_folder = int(folder)
-    max_folder += 1
-    print(max_folder)
-    #mkdir dataset_path + '/' + str(max_folder)
-    
-    generate_random_dataset(dataset_path + '/' + str(max_folder))
 
-if __name__ == '__main__':
-    load_dataset()
-    exit(0)
-    # img, label =
-    # 数据集路径
-    data_path = "/path/to/your/dataset"
-    # 假设有一个包含图像文件和标签的数据集，可以使用transforms进行图像预处理
     transform = transforms.Compose([
-        transforms.Resize((64, 64)),
+        transforms.Resize((256, 256)),
         transforms.ToTensor(),
     ])
-    # 创建自定义数据集实例
-    custom_dataset = CustomDataset(img, label, transform=transform)
-    #
-    # 使用 DataLoader 加载数据
-    batch_size = 10
-    data_loader = DataLoader(
-        custom_dataset, batch_size=batch_size, shuffle=True)
+    custom_dataset = CustomDataset(dataset_path, transform=transform)
+    return custom_dataset
 
+def train(dataset):
+    batch_size = 2
+    data_loader = DataLoader(
+        dataset, batch_size=batch_size, shuffle=True)
     model = cnn.Densenet(layer=121, pretrained=True).to(device)
     optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-    # 训练
-    for epoch in range(100):
-        for images, labels in data_loader:
+    for epoch in range(50):
+        print("Epoch:", epoch)
+        for image, label in data_loader:
+            model.train()
             optimizer.zero_grad()
-            # images = images.to(device)
-            # labels = labels.to(device)
-            # 前向传播
-            images = images.type(torch.FloatTensor).to(device)
-            outputs = model(images)
+            image , label = image.to(device), label.to(device)
+            outputs = model(image)
             criterion = torch.nn.MSELoss()
             # 计算损失
-            loss = criterion(outputs.double(), labels.to(device).double())
+            loss = criterion(outputs.double(), label.to(device).double())
             # 反向传播,adam优化
             loss.backward()
             optimizer.step()
             print(loss.item())
 
-    for images, labels in data_loader:
-        outputs = model(images.type(torch.FloatTensor))
-        print("outputs:", outputs)
-        print("labels:", labels)
-        break
+    #save model
+    torch.save(model, 'model.pth')
+
+def evaluate(model, dataset):
+    batch_size = 1
+    data_loader = DataLoader(
+        dataset, batch_size=batch_size, shuffle=True)
+    for image, label in data_loader:
+        image, label = image.to(device), label.to(device)
+        outputs = model(image)
+        print(outputs)
+        print(label)
+        print(torch.mean(torch.abs(outputs - label)))
+        print("")
+
+if __name__ == '__main__':
+    dataset = load_dataset()
+    # model = torch.load('model.pth')
+    # evaluate(model, dataset)
+    train(dataset)
--- imitation_learning/video2dataset.py
+++ imitation_learning/video2dataset.py
@@ -1,3 +1,6 @@
 import cv2 as cv
 import numpy as np
 import os
+import rospy
+
+from sensor_msgs.msg import Image
