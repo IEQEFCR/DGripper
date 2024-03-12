b52b6f6be99f6082b25f9a90086b816ee0d9a2fb
ieqefcr <ieqefcr@hotmail.com>
2024-03-12 14:46:17 +0800

    tactile merged

--- DGripper_ws/src/shape_reconstruction/script/_3_Shape_Reconstruction.py
+++ DGripper_ws/src/shape_reconstruction/script/_3_Shape_Reconstruction.py
@@ -1,29 +1,67 @@
+import os
 import cv2
+import numpy as np
 import yaml
 from sensor import Sensor
 from visualizer import Visualizer
 
+def tactile_img(sensor):
+    img = sensor.get_rectify_crop_image()
+    print(img.shape)
+    img_GRAY = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    cv2.imshow('RawImage_GRAY', img_GRAY)
+    height_map = sensor.raw_image_2_height_map(img_GRAY)
+    depth_map = sensor.height_map_2_depth_map(height_map)
+    cv2.imshow('DepthMap', depth_map)
+    height_map = sensor.expand_image(height_map)
+    # depth_map_video.write(cv2.cvtColor(depth_map, cv2.COLOR_GRAY2BGR))
+    return depth_map
+
 if __name__ == '__main__':
+
+    camera = cv2.VideoCapture(0)
+    camera.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+    camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+    #set camera to grayscale
+    #change to the directory of the script
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
     f = open("shape_config.yaml", 'r+', encoding='utf-8')
     cfg = yaml.load(f, Loader=yaml.FullLoader)
-    sensor = Sensor(cfg)
-    visualizer = Visualizer(sensor.points)
+    cfg['camera_setting']['camera_channel'] =2
+    sensor1 = Sensor(cfg)
+    cfg['camera_setting']['camera_channel'] =4
+    sensor2 = Sensor(cfg)
+    index = 0
+    # visualizer = Visualizer(sensor.points)
     depth_map_video = cv2.VideoWriter('depth_map.mp4', cv2.VideoWriter_fourcc(*'MJPG'), 20, (460,345))
-    while sensor.cap.isOpened():
-        img = sensor.get_rectify_crop_image()
-        img_GRAY = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        cv2.imshow('RawImage_GRAY', img_GRAY)
-        height_map = sensor.raw_image_2_height_map(img_GRAY)
-        depth_map = sensor.height_map_2_depth_map(height_map)
-        cv2.imshow('DepthMap', depth_map)
-        height_map = sensor.expand_image(height_map)
-        depth_map_video.write(cv2.cvtColor(depth_map, cv2.COLOR_GRAY2BGR))
+    while sensor1.cap.isOpened() and sensor2.cap.isOpened():
+        t1 = tactile_img(sensor1)
+        t2 = tactile_img(sensor2)
+        show_img = np.zeros((345,460,3), np.uint8)
+        camera_img = camera.read()[1]
+        #cvt to grayscale
+        camera_img_GRAY = cv2.cvtColor(camera_img, cv2.COLOR_BGR2GRAY)
+        # cv2.imshow('Camera', camera_img_GRAY)
+        camera_img_GRAY = cv2.resize(camera_img_GRAY, (460,345))
+        #show to 3channel
+        # show_img = cv2.cvtColor(t1, cv2.COLOR_GRAY2BGR)
+        show_img[:,:,0] = t1.astype(np.uint8)*2
+        show_img[:,:,2] = t2.astype(np.uint8)*2
+        show_img[:,:,1] = camera_img_GRAY//2
+        # show_img = cv2.cvtColor(t2, cv2.COLOR_GRAY2BGR)
+        #
+        #print type of show_img
+        # print(show_img)
+        cv2.imshow('Merged', show_img)
         key = cv2.waitKey(1)
+        if key == ord('s'):
+            cv2.imwrite(str(index)+'_merged.png', show_img)
+            index += 1
         if key == ord('q'):
             break
-        if not visualizer.vis.poll_events():
-            break
-        else:
-            points, gradients = sensor.height_map_2_point_cloud_gradients(
-                height_map)
-            visualizer.update(points, gradients)
+        # if not visualizer.vis.poll_events():
+        #     break
+        # else:
+        #     points, gradients = sensor.height_map_2_point_cloud_gradients(
+        #         height_map)
+        #     visualizer.update(points, gradients)
--- yolo.py
+++ yolo.py
@@ -1,11 +1,11 @@
 import rospy
 import cv2 as cv
-
+from ultralytics import YOLO
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 
 def cv_image_callback(msg):
-    print('Received an image!')
+    # print('Received an image!')
     global cv_image
     #rotate image
     cv_image = cv.rotate(bridge.imgmsg_to_cv2(msg, "bgr8"), cv.ROTATE_90_CLOCKWISE)
@@ -15,9 +15,24 @@ if __name__ == '__main__':
     bridge = CvBridge()
     cv_image = None
     image_sub = rospy.Subscriber('/usb_cam/image_raw', Image, cv_image_callback)
+    
+    # Load YOLO
+    model = YOLO('best.pt')
+
     cv.namedWindow('yolo', cv.WINDOW_NORMAL)
     while not rospy.is_shutdown():
         if cv_image is not None:
-            cv.imshow('yolo', cv_image)
+            
+            # Detect
+            result = model.predict(cv_image)[0].cpu()
+            if result.boxes :
+                print(result)
+                for i in result.boxes:
+                    box = i.xywh[0]
+                    cv_image =cv.rectangle(cv_image, (int(box[0]-box[2]/2), int(box[1]-box[3]/2)), (int(box[0]+box[2]/2), int(box[1]+box[3]/2)), (0, 255, 0), 2)
+                    class_name = model.names[int(i.cls)]
+                    cv.putText(cv_image, class_name, (int(box[0]-box[2]/2), int(box[1]-box[3]/2)), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+            # print(result)
+        cv.imshow('yolo', cv_image)
         cv.waitKey(1)
     cv.destroyAllWindows()
